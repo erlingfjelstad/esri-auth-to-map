8fccde6165143deb28fca0a98b28463666451768
Erling Fjelstad <erlingfjelstad@tgmail.com>
2024-06-12 12:29:02 +0200

    Apply suggestion from Erick_1

--- app/src/main/java/no/geodata/authenticatetomap/MainActivity.kt
+++ app/src/main/java/no/geodata/authenticatetomap/MainActivity.kt
@@ -1,93 +1,51 @@
 package no.geodata.authenticatetomap
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
-import no.geodata.authenticatetomap.map.MapView
-import no.geodata.authenticatetomap.ui.theme.AuthenticateToMapTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         val viewModel = MainViewModel()
         setContent {
-            AuthenticateToMapTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LaunchedEffect(key1 = LocalLifecycleOwner.current) {
+            val coroutineScope = rememberCoroutineScope()
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Button(modifier = Modifier.fillMaxWidth(), onClick = {
+                    Log.d("ArcGIS-Main", "onCreate: $innerPadding")
+                    coroutineScope.launch {
                         viewModel.authenticate()
                     }
-                    val uiState by viewModel.mapFlow.collectAsState()
-                    when (val state = uiState) {
-                        is UiState.Error -> {
-                            val coroutineScope = rememberCoroutineScope()
-                            Column(
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .padding(innerPadding)
-                                    .padding(16.dp),
-                                verticalArrangement = Arrangement.spacedBy(8.dp)
-                            ) {
-                                Text(text = state.title)
-                                Text(text = state.text)
-                                Button(onClick = {
-                                    coroutineScope.launch {
-                                        viewModel.authenticate()
-                                    }
-                                }) {
-                                    Text(text = "Retry load map")
-                                }
-                            }
-                        }
+                }) {
+                    Text("Authenticate")
+                }
+                val uiState by viewModel.mapFlow.collectAsState()
+                when (val state = uiState) {
+                    is UiState.Error -> {
+                        Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+                        Log.d("ArcGIS-Main", "onCreate: Error ${state.text}")
+                        Log.d("ArcGIS-Main", "onCreate: Error ${state.title}")
+                    }
 
-                        UiState.Loading -> {
-                            Box(
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .padding(innerPadding),
-                                contentAlignment = Alignment.Center
-                            ) {
-                                Column(
-                                    modifier = Modifier.fillMaxWidth(),
-                                    horizontalAlignment = Alignment.CenterHorizontally,
-                                    verticalArrangement = Arrangement.spacedBy(8.dp)
-                                ) {
-                                    CircularProgressIndicator()
-                                    Text(text = "Loading map…")
-                                }
-                            }
-                        }
+                    UiState.Loading -> {
+                        Log.d("ArcGIS-Main", "onCreate: LOADING")
+                    }
 
-                        is UiState.Success -> {
-                            MapView(
-                                arcGISMap = state.model,
-                                modifier = Modifier
-                                    .fillMaxSize()
-                                    .padding(innerPadding)
-                            )
-                        }
+                    is UiState.Success -> {
+                        Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show()
+                        Log.d("ArcGIS-Main", "onCreate: Success $uiState")
                     }
                 }
             }
